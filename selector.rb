8cc1ebd29a48f8be6badbbb1499d44b4fb697159
brennx0r <brennx0r@gmail.com>
2016-08-23 21:04:28 -0700

    Little ruby program that will select the tsumtsum and modify the feed list to remove what's been selected.

---
+++ selector.rb
@@ -0,0 +1,21 @@
+# read in the text file and save values as an array
+def arraycreate(filename)
+	array = IO.readlines(filename)
+	selector(array, filename)
+end
+
+# select the tsumtsum using sample method
+def selector(array, filename)
+	selected = array.sample
+	modifyfile(selected, filename)
+end
+
+# delete the selected item from the text file
+def modifyfile(selected, filename)
+	filetext = File.read(filename) 
+	new_contents = filetext.gsub(selected, "" )
+	File.open(filename, "w") { |file| file.puts new_contents }
+end
+
+filename = "list.txt".to_s
+arraycreate(filename)
